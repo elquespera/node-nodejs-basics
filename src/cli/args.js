034e9c744b2ff22427893a0e61c8c748fd5a7346
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-04 08:36:37 +0200

    feat: implement env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,4 @@
 export const parseArgs = () => {
-    //console.log(p);
     console.log(process.argv.slice(2).
                 map((arg, i) => i % 2 ? arg : arg.slice(2)).
                 reduce((t, arg, i) => t + arg + (i % 2 ? ', ' : ' is '), '').
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,7 @@
 export const parseEnv = () => {
-    // Write your code here 
+    let env = Object.entries(process.env);
+    env = env.filter(([key, value]) => key.indexOf('RSS_') === 0);
+    console.log(env.map(([key, value]) => `${key}=${value}`).join('; ')); 
 };
+
+parseEnv();
