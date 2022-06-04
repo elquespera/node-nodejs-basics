b257875811a9c67e49b221f1c24db18907fe9124
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-04 10:16:59 +0200

    feat: implement fs read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,15 @@
+import  *  as fs from 'node:fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+    const f_name = './files/fileToRead.txt';  
+    try {
+        console.log(await fs.readFile(f_name, { encoding: 'utf8' }));
+    }
+    catch(error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } 
+        else
+            throw error;
+    }
 };
