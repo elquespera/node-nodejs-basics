cc9dc412c293cd3973c6ac095ec2cb35c296bf0c
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-05 15:00:26 +0200

    feat: cp add explicit silent = false

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -6,7 +6,7 @@ const __dir = path.dirname(fileURLToPath(import.meta.url));
 import * as cp from 'child_process';
 
 export const spawnChildProcess = async (args) => {    
-    cp.fork(path.resolve(__dir, 'files/script.js'), process.argv);
+    cp.fork(path.resolve(__dir, 'files/script.js'), process.argv, {silent: false});
 };
 
 //Testing
