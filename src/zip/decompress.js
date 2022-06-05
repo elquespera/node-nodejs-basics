d19b25f5b526e3c720f35da8e2929b9bda440e71
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-06-05 04:02:33 +0200

    feat: implement decompress.js form gzip folder

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,30 @@
+// Get current dir
+import { fileURLToPath } from 'node:url';
+import * as path from 'node:path';
+const __dir = path.dirname(fileURLToPath(import.meta.url));
+
+import * as stream from "node:stream/promises";
+import * as fs from 'node:fs/promises';
+import { createGunzip } from 'node:zlib'
+
+
 export const decompress = async () => {
-    // Write your code here 
+    const from = path.resolve(__dir, 'files/archive.gz');
+    const to = path.resolve(__dir, 'files/fileToCompress.txt');    
+    try {           
+        const sourceHandle = await fs.open(from, 'r');
+        const destHandle = await fs.open(to, 'w');
+        await stream.pipeline(
+            sourceHandle.createReadStream(from),
+            createGunzip(),
+            destHandle.createWriteStream(to)
+        );
+    }
+    catch(error) {
+        throw error;
+    }    
 };
+
+
+//For testing
+decompress();
